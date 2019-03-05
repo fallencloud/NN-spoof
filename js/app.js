3c940ba2b769d8d63f99c508e0994d15b1f491c9
Sharina V. Jones <sharinavjones@gmail.com>
2019-03-05 06:16:54 -0500

    adding oo js

--- js/app.js
+++ js/app.js
@@ -45,47 +45,65 @@ class EventsClass {
   generateEventHtml(event) {
     return `
     <div class="card">
-                <div class="card-header">
-                  <div class="container-fluid mx-0 px-0">
-                    <div class="row">
-                      <div class="col-3">
-                        <h2><span class="badge badge-secondary">${
-                          event.date
-                        }</span></h2>
-                        <p class="lead">${event.month}</p>
-                      </div>
-                      <div class="col-9">
-                        <h5 class="text-uppercase display-5">
-                          ${event.title}
-                        </h5>
-                        <ul class="list-inline">
-                          <li class="list-inline-item">
-                            <i class="fa fa-calendar-o" aria-hidden="true"></i>
-                            ${event.day}
-                          </li>
-                          <li class="list-inline-item">
-                            <i class="fa fa-clock-o" aria-hidden="true"></i>
-                            ${event.time}
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="card-body">
-                  <h5 class="card-title">Special title treatment</h5>
-                  <p class="card-text">
-                    With supporting text below as a natural lead-in to
-                    additional content.
-                  </p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-              </div>
+      <div class="card-header">
+        <div class="container-fluid mx-0 px-0">
+          <div class="row">
+            <div class="col-3">
+              <h2><span class="badge badge-secondary">${event.date}</span></h2>
+              <p class="lead">${event.month}</p>
+            </div>
+            <div class="col-9">
+              <h5 class="text-uppercase display-5">
+                ${event.title}
+              </h5>
+              <ul class="list-inline">
+                <li class="list-inline-item">
+                  <i class="fa fa-calendar-o" aria-hidden="true"></i>
+                  ${event.day}
+                </li>
+                <li class="list-inline-item">
+                  <i class="fa fa-clock-o" aria-hidden="true"></i>
+                  ${event.time}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">
+          With supporting text below as a natural lead-in to
+          additional content.
+        </p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
     `;
   }
 } //end class EventsClass
 
-class NewsClass {}
+class NewsClass {
+  constructor() {
+    this.news = [
+      {
+        title: "Newport News and Partners to Host Regional Job Fair",
+        summary:
+          "To celebrate the dedication of the Four Oaks Day Service Center, the City of Newport News, Volunteers of America Chesapeake, Virginia Career Works - Greater Peninsula, and the Virginia Employment Commission are hosting a Job Fair on Tuesday, March 5, 2019..."
+      },
+      {
+        title: "MARATHON WEEKEND BEGINS TOMORROW IN NEWPORT NEWS ",
+        summary:
+          "The City of Newport News’ fifth annual One City Marathon takes place Sunday and, with related events all weekend long, there are fun activities..."
+      },
+      {
+        title: "JOB FAIRS FOR SUMMER PARKS & RECREATION POSITIONS",
+        summary:
+          "The Newport News Department of Parks, Recreation and Tourism is holding three job fairs next month to fill over 180 summer positions in various locations throughout the City. Some yea..."
+      }
+    ];
+  }
+}
 
 window.addEventListener("load", () => {
   events = new EventsClass();
